8c117a1d4bb6ac852530e45d549a765511b275d2
Wibson82 <67915780+Wibson82@users.noreply.github.com>
2022-01-20 17:12:07 -0300

    Criamos o Find Category By id, excpetion handle

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+import java.io.Serial;
+
+public class EntityNotFoundException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public EntityNotFoundException(String msg){
+        super(msg);
+    }
+
+    public EntityNotFoundException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+}
